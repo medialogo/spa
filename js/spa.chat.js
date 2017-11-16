57fac040b32d0d50123f43a9bfe32f5b01447081
t.ikemi (1015) <ikemi@medialogo.com>
2017-11-17 01:26:10 +0900

    up to spa.chat.js setPxSizes, p142

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -18,29 +18,106 @@ spa.chat = (function () {
   var
     configMap = {
       main_html : String()
-        + '<div style="padding:1em; color*#fff;">'
-        + 'なにかしゃべってみましょう'
-        + '</div>',
-      settable_map : {},
+        + '<div class="spa-chat">'
+          + '<div class="spa-chat-head">'
+            + '<div class="spa-chat-head-toggle">+</div>'
+            + '<div class="spa-chat-head-title">'
+              + 'チャット'
+            + '</div>'
+          + '</div>'
+          + '<div class="spa-chat-closer">x</div>'
+          + '<div class="spa-chat-sizer">'
+          + '<div class="spa-chat-msgs"></div>'
+          + '<div class="spa-chat-box">'
+          + '<input type="text">'
+          + '<div>送信</div>'
+         + '</div>'
+        + '</div>'
+      + '</div>',
+      
+      settable_map : {
+        slider_open_time : true,
+        slider_close_time : true,
+        slider_opened_em : true,
+        slider_closed_em : true,
+        slider_opened_title : true,
+        slider_closed_title : true,
+        
+        chat_model	: true,
+        people_model : true,
+        set_chat_anchor : true
+      },
+      
+      slider_open_time 	: 250,
+      slider_close_time : 250,
+      slider_opened_em	: 16,
+      slider_closed_em	: 2,
+      slider_opened_title : 'クリックして閉じる',
+      slider_closed_title : 'クリックして開く',
+      
+      chat_model	: null,
+      people_model	: null,
+      set_chat_anchor : null
     },
-    stateMap  = { $container : null },
+    stateMap  = { 
+	  $append_target : null 
+	  position_type  : 'closed',
+	  px_per_em		 : 0,
+	  slider_hidden_px : 0,
+	  slider_closed_px : 0,
+	  slider_opened_px : 0
+    },
     jqueryMap = {},
 
-    setJqueryMap, configModule, initModule;
+    setJqueryMap, getEmSize, setPxSizes, setSliderPosition,
+    onClickToggle, configModule, initModule;
+  
   //----------------- モジュールスコープ変数↑ ---------------
 
   //------------------- ユーティリティメソッド↓ ------------------
-  // example : getTrimmedString
+  //
+  getEmSize = function ( elem ) {
+    return Number(
+      getComputedStyle( elem, '').fontSize.match(/\d*\.?\d*/)[0]
+    );
+  }
   //-------------------- ユーティリティメソッド↑ -------------------
 
   //--------------------- DOMメソッド↓ --------------------
   // DOM メソッド /setJqueryMap/↓
   setJqueryMap = function () {
-    var $container = stateMap.$container;
+    var 
+      $append_target = stateMap.$append_target;
+      $slider = $append_target.find( '.spa-chat');
 
-    jqueryMap = { $container : $container };
+    jqueryMap = { 
+      $slider   : $slider,
+      $head		: $slider.find('.spa-chat-head'),
+      $toggle	: $slider.find('.spa-chat-head-toggle'),
+      $title	: $slider.find('.spa-chat-head-title'),
+      $sizer	: $slider.find('.spa-chat-sizer'),
+      $msgs		: $slider.find('.spa-chat-msgs'),
+      $box		: $slider.find('.spa-chat-box'),
+      $input 	: $slider.find('.spa-chat-input input[type=tex]') };
   };
   // DOM メソッド /setJqueryMap/ ↑
+  
+  // DOM メソッド /setPxSize/↓
+  setPxSize - function () {
+	var px_per_em, opened_height_em;
+	px_per_em = getEmSize ( jqueryMap.$slider.get(0) );
+	
+	opened_height_em = configMap.slider_opened_em;
+	
+	stateMap.px_per_em = px_per_em;
+	stateMap.slider_closed_px = configMap.slider_closed_em * px_per_em;
+	stateMap.slider_opened_px = opened_height_em * px_per_em;
+	jqueryMap.$sizer.css({
+	  height ; ( opened_height_em - 2 ) * px_per_em
+	})
+  };
+  // DOM メソッド /setPxSize/↑
+  
   //---------------------- DOMメソッド↑ ---------------------
 
   //------------------- イベントハンドラ↓ -------------------
@@ -95,6 +172,24 @@ spa.chat = (function () {
     return true;
   };
   // パブリックメソッド /initModule/ ↑
+
+  // パブリックメソッド /setSliderPosition/ ↓
+  //
+  // 用例 : spa.chat.setSliderPosition( 'closed' );
+  // 目的 : チャットスライダーが要求された状態になるようにする
+  // 引数 :
+  //  * position_type - enum('closed', 'opened', または 'hidden')
+  //  * callback - アニメーションの最後のオプションのコールバック
+  //    (コールバックは引数としてスライダーDOM要素を受け取る)
+  // 動作 :
+  //   スライダーが要求に合致している場合は現在の状態のままにする。
+  //   それ以外の場合はアニメーションを使って要求された状態にする。
+  // 戻り値 :
+  //   * true - 要求された状態を実現した
+  //   * false - 要求された状態を実現していない
+  // 例外発行 : なし
+  //
+  // パブリックメソッド /setSliderPosition/ ↑
 
   // パブリックメソッドを返す
   return {
